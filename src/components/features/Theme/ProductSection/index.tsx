e62d8190a16b48a2b05121f763ca309e72729b61
yuni <hys990730@gmail.com>
2024-07-11 20:10:50 +0900

    refactor: ProductSection 컴포넌트에 useFetch 훅 적용

--- src/components/features/Theme/ProductSection/index.tsx
+++ src/components/features/Theme/ProductSection/index.tsx
@@ -1,9 +1,8 @@
 import styled from '@emotion/styled';
-import { useEffect, useState } from 'react';
 
 import { Loading } from '@/api/Loading';
 import { NoData } from '@/api/NoData';
-import { fetchThemeProduct } from '@/api/themeApi';
+import { useFetch } from '@/api/useFetch';
 import { DefaultItems } from '@/components/common/Item/Default';
 import { Container } from '@/components/common/layouts/Container';
 import { Grid } from '@/components/common/layouts/Grid';
@@ -15,32 +14,22 @@ type Props = {
 };
 
 export const ProductSection = ({ themeKey }: Props) => {
-  const [products, setProducts] = useState<ProductData[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-
-  useEffect(() => {
-    const getProducts = async () => {
-      setIsLoading(true);
-      try {
-        const data = await fetchThemeProduct(themeKey, 20); // Request 20 products
-        setProducts(data.products);
-      } catch (error) {
-        console.error('Failed to fetch theme products', error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    getProducts();
-  }, [themeKey]);
+  const {
+    data: products,
+    isLoading,
+    error,
+  } = useFetch<{ products: ProductData[] }>('/api/v1/themes/${themeKey}/products', {
+    maxResults: 20,
+  });
 
   if (isLoading) {
     return <Loading />;
   }
 
-  if (products.length === 0) {
+  if (error || !products || products.products.length === 0) {
     return <NoData />;
   }
+
   return (
     <Wrapper>
       <Container>
@@ -51,7 +40,7 @@ export const ProductSection = ({ themeKey }: Props) => {
           }}
           gap={16}
         >
-          {products.map(({ id, imageURL, name, price, brandInfo }) => (
+          {products.products.map(({ id, imageURL, name, price, brandInfo }) => (
             <DefaultItems
               key={id}
               imageSrc={imageURL}
